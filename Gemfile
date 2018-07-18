e2b65ee5fe0d93a87f2eb0611023a96255d6b4ea
trang <trangnguyen1801hs@gmail.com>
2018-07-18 15:23:16 +0700

    Sign up!

--- Gemfile
+++ Gemfile
@@ -13,7 +13,7 @@ gem "uglifier", ">= 1.3.0"
 gem "coffee-rails", "~> 4.2"
 gem "turbolinks", "~> 5"
 gem "jbuilder", "~> 2.5"
-gem 'bootstrap-sass', '3.3.7'
+gem "bootstrap-sass", "3.3.7"
 gem "jquery-rails"
 
 gem "bootsnap", ">= 1.1.0", require: false
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
-  def new
+  def show
   end
+
+  def new
+    @user = User.new
+  end
+
+   def create
+    @user = User.new params[:user]
+    if @user.save
+      flash[:success] = t".flash"
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+  def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+  end
+
+  private
+
+  def find_by
+    @user = User.find_by id: params[:id]
+    return if @user
+      flash[:success] = t :not_flash
+      redirect_to @user
+  end
+
 end
